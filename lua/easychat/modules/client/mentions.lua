612acc535aa703ee8dca0c9987c8ef08791423e3
Earu <reno.reboursier@gmail.com>
2017-08-28 11:51:02 +0200

    Update
    Structure change
    Browser top open links from ec
    Make chatbox panel, a scripted one
    Make global chat tab a scripted panel
    
    todo:
    -fixes on chathud
    -move setting tab to a scripted panel

---
+++ lua/easychat/modules/client/mentions.lua
@@ -0,0 +1,25 @@
+local Mention = function(arg,txt)
+    if not LocalPlayer().Nick then return end
+    local str = string.lower(txt)
+    local lname = string.lower(string.gsub(LocalPlayer():Nick(),"<.->",""))
+    if not string.match(str,"^[%!|%.|%/]") and string.match(str,string.PatternSafe(lname)) then
+        if not system.HasFocus() then
+            system.FlashWindow()
+        end
+        if type(arg) == "string" then
+            if string.TrimLeft(arg) == "" or string.lower(arg) == lname then return end
+            chat.AddText(Color(114,137,218),"[Discord] "..arg,Color(255,255,255),": "..txt)
+            return false --hide discord message
+        else
+            if not IsValid(arg) then return end
+            if lname == string.lower(string.gsub(arg:Nick(),"<.->","")) then return end
+            chat.AddText(arg,Color(255,255,255),": ",Color(244, 167, 66),txt)
+            return true -- hide chat message
+        end
+    end
+end
+
+hook.Add("OnPlayerChat","EasyChatModuleMention",Mention)
+hook.Add("OnDiscordMessage","EasyChatModuleMention",Mention)
+
+return "Mentions"
