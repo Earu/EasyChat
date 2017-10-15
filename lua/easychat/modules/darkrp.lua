fdc47e72d7ba1da99c5c65cc0b2baac872ad277a
R.Réno <earu.derg@gmail.com>
2017-10-15 23:56:22 +0200

    add me,broadcast,pm,group

--- lua/easychat/modules/darkrp.lua
+++ lua/easychat/modules/darkrp.lua
@@ -24,18 +24,36 @@ if CLIENT then
 
     local modes = {
         ooc = function(ply,txt)
-            chat.AddText(Color(125,125,125),"[OOC] ",ply,Color(255,255,255),":"..txt)
+            chat.AddText(Color(125,125,125),"[OOC] ",ply,Color(255,255,255),": "..txt)
         end,
         ["/"] = modes.ooc, --alias in darkrp
+        a = mode.ooc, --another ooc alias
         advert = function(ply,txt)
             chat.AddText(Color(244,167,66),"►---------◄ Advert By "..ply:GetName().." ►---------◄")
             chat.AddText(Color(244,167,66),"⮞⮞ ",Color(255,255,255),txt)
         end,
         y = function(ply,txt)
-            chat.AddText(Color(240,125,125),"[Yell] ",ply,Color(255,255,255),":"..txt)
+            chat.AddText(Color(240,125,125),"[Yell] ",ply,Color(255,255,255),": " .. txt)
         end,
         w = function(ply,txt)
-            chat.AddText(Color(75,75,240),"[Whisper] ",ply,Color(255,255,255),":"..txt)
+            chat.AddText(Color(75,75,240),"[Whisper] ",ply,Color(255,255,255),": " .. txt)
+        end,
+        me = function(ply,txt)
+            chat.AddText(Color(255,255,255),"**" .. ply .. " " .. txt .. "**")
+        end,
+        broadcast = function(ply,txt)
+            chat.AddText(Color(150,20,20),"[Broadcast] ",ply,Color(255,255,255),": " .. txt)
+        end,
+        pm = function(ply,txt)
+            local parts = string.Explode(" ",txt)
+            if string.match(parts[1],LocalPlayer():GetName()) then
+                chat.AddText(Color(220,220,60),"[PM] ",ply,Color(255,255,255),": " .. table.concat(parts," ",2,#parts))
+            end
+        end,
+        group = function(ply,txt)
+            if ply:Team() == LocalPlayer():Team() then
+                chat.AddText(Color(220,60,220),"[Group] ",ply,Color(255,255,255),": " .. txt)
+            end
         end,
     }
 
