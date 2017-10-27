ce8523f35af6f1a380a7c1f9e0d878960409797a
R.Réno <reno.reboursier@gmail.com>
2017-10-27 20:09:03 +0200

    add a convar to disable join leave messages to module

--- lua/easychat/modules/joinleave.lua
+++ lua/easychat/modules/joinleave.lua
@@ -1,20 +1,24 @@
-local tag = "EasyChatModuleJoinLeave"
+local TAG = "EasyChatModuleJoinLeave"
 
 if SERVER then
 
-    util.AddNetworkString(tag)
+    local EC_JOIN_LEAVE = CreateConVar("easychat_joinleave_msg","1",FCVAR_ARCHIVE,"Enables or disables join/leave messages")
+
+    util.AddNetworkString(TAG)
 
     gameevent.Listen("player_connect")
     gameevent.Listen("player_disconnect")
 
-    hook.Add("player_connect", tag, function(data)
-        net.Start(tag)
+    hook.Add("player_connect", TAG, function(data)
+        if not EC_JOIN_LEAVE:GetBool() then return end
+        net.Start(TAG)
         net.WriteTable(data)
         net.Broadcast()
     end)
 
-    hook.Add("player_disconnect", tag, function(data)
-        net.Start(tag)
+    hook.Add("player_disconnect", TAG, function(data)
+        if not EC_JOIN_LEAVE:GetBool() then return end
+        net.Start(TAG)
         net.WriteTable(data)
         net.Broadcast()
     end)
@@ -23,13 +27,13 @@ end
 
 if CLIENT then
 
-    hook.Add("ChatText", tag, function(_, _, _, mode)
+    hook.Add("ChatText", TAG, function(_, _, _, mode)
         if mode == "joinleave" then
             return true
         end
     end)
 
-    net.Receive(tag, function()
+    net.Receive(TAG, function()
         local info = net.ReadTable()
         if not info.reason then
             chat.AddText(Color(127, 255, 127), "⮞ ", Color(200, 200, 200), info.name, Color(175, 175, 175), " (" .. info.networkid .. ") is ", Color(127, 255, 127), "joining")
