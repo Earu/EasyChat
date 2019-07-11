1327b1d572463ebc9b335607c412224d6967f875
Earu <earu.derg@gmail.com>
2019-07-11 13:38:39 +0200

    Fix compat with murder and various other things

---
+++ lua/easychat/modules/murder.lua
@@ -0,0 +1,9 @@
+if gmod.GetGamemode().Name ~= "Murder" then return end
+
+if SERVER then
+    hook.Add("PlayerSay","EasyChatMurderWorkaround",function(ply, msg)
+        if ply:Team() ~= 2 or not ply:Alive() or GAMEMODE:GetRound() == 0 then
+            return msg
+        end
+    end)
+end
