5f6ca84bdf57773fd5e245947b85acbede722ca2
Earu <earu.derg@gmail.com>
2019-07-11 18:03:12 +0200

    Add history and admin chat

---
+++ lua/easychat/modules/admin_tab.lua
@@ -0,0 +1,132 @@
+local EASYCHAT_ADMIN = "EASY_CHAT_MODULE_ADMIN"
+
+if SERVER then
+    util.AddNetworkString(EASYCHAT_ADMIN)
+
+    net.Receive(EASYCHAT_ADMIN, function(_, ply)
+        if not ply:IsAdmin() then return end
+        local msg = net.ReadString()
+        msg = msg:Trim()
+        if msg == "" then return end
+
+        local admins = {}
+        for _,p in ipairs(player.GetAll()) do
+            if p:IsAdmin() then
+                table.insert(admins,p)
+            end
+        end
+
+        net.Start(EASYCHAT_ADMIN)
+        net.WriteEntity(ply)
+        net.WriteString(msg)
+        net.Send(admins)
+    end)
+end
+
+if CLIENT then
+    local PLY_COL = Color(255,127,127)
+
+    local ADMIN_TAB = {
+        NewMessages = 0,
+        Init = function(self)
+            local frame = self
+
+            self.TextEntry = self:Add("DTextEntry")
+            self.TextEntry:SetTall(20)
+            self.TextEntry:Dock(BOTTOM)
+            self.TextEntry:SetHistoryEnabled(true)
+            self.TextEntry.HistoryPos = 0
+            self.TextEntry:SetUpdateOnType(true)
+
+            self.RichText = self:Add("RichText")
+            self.RichText.HistoryName = "admin"
+            if not EasyChat.UseDermaSkin then
+                self.RichText:InsertColorChange(255,255,255,255)
+            end
+            self.RichText.PerformLayout = function(self)
+                self:SetFontInternal("EasyChatFont")
+                if not EasyChat.UseDermaSkin then
+                    self:SetFGColor(EasyChat.TextColor)
+                end
+            end
+            self.RichText.ActionSignal = function(self,name,value)
+                if name == "TextClicked" then
+                    EasyChat.OpenURL(value)
+                end
+            end
+            self.RichText:Dock(FILL)
+
+            local lastkey = KEY_ENTER
+            self.TextEntry.OnKeyCodeTyped = function(self,code)
+                EasyChat.SetupHistory(self,code)
+                EasyChat.UseRegisteredShortcuts(self,lastkey,code)
+
+                if code == KEY_ESCAPE then
+                    chat.Close()
+                    gui.HideGameUI()
+                elseif code == KEY_ENTER or code == KEY_PAD_ENTER then
+                    self:SetText(string.Replace(self:GetText(),"╚​",""))
+                    if string.Trim(self:GetText()) ~= "" then
+                        frame:SendMessage(string.sub(self:GetText(),1,3000))
+                    end
+                end
+
+                lastkey = code
+            end
+
+            local history = EasyChat.ReadFromHistory("admin")
+            if string.Trim(history) == "" then
+                EasyChat.AddText(self, self.RichText, "Welcome to the admin chat!")
+            else
+                self.RichText:AppendText(history) -- so we do not log twice
+                EasyChat.AddText(self, self.RichText, "\n^^^^^ Last Session History ^^^^^\n\n")
+                self.RichText:GotoTextEnd()
+            end
+        end,
+        Notify = function(self,message)
+            self.NewMessages = self.NewMessages + 1
+            EasyChat.FlashTab("Admin")
+            _G.chat.AddText(Color(255,255,255),"[Admin Chat | ",Color(255,127,127),chat.Player,Color(255,255,255),"] " .. message)
+        end,
+        SendMessage = function(self, msg)
+            net.Start(EASYCHAT_ADMIN)
+            net.WriteString(msg)
+            net.SendToServer()
+            self.TextEntry:SetText("")
+        end,
+    }
+
+    vgui.Register("ECAdminTab", ADMIN_TAB, "DPanel")
+    local admintab = vgui.Create("ECAdminTab")
+    local admins = {}
+
+    net.Receive(EASYCHAT_ADMIN,function()
+        local sender = net.ReadEntity()
+        local msg = net.ReadString()
+        if not IsValid(sender) then return end
+
+        EasyChat.AddText(admintab, admintab.RichText, team.GetColor(sender:Team()), sender,": " .. msg)
+        if not EasyChat.IsOpened() then
+            admintab:Notify(msg)
+        else
+            local activetabname = EasyChat.GetActiveTab().Tab.Name
+            if activetabname ~= "Admin" then
+                admintab:Notify(msg)
+            end
+        end
+    end)
+
+    hook.Add("ECTabChanged","EasyChatModuleDMTab",function(_,tab)
+        if tab == "Admin" then
+            admintab.NewMessages = 0
+            if not LocalPlayer():IsAdmin() then
+                EasyChat.AddText(self, self.RichText, "You cannot see the content of this channel because you are not an admin")
+            end
+        end
+    end)
+
+    EasyChat.AddTab("Admin", admintab)
+    EasyChat.SetFocusForOn("Admin", admintab.TextEntry)
+end
+
+return "Admin Chat"
