038934d4ce8e6d37fb9db8a06ca8a7819fa9cda3
Earu <earu.derg@gmail.com>
2019-12-25 20:11:30 +0100

    Move default_tags to a module

--- lua/autorun/easychat_init.lua
+++ lua/autorun/easychat_init.lua
@@ -7,7 +7,6 @@ AddCSLuaFile("easychat/client/chat_tab.lua")
 AddCSLuaFile("easychat/client/settings_tab.lua")
 AddCSLuaFile("easychat/client/browser_panel.lua")
 AddCSLuaFile("easychat/client/chathud.lua")
-AddCSLuaFile("easychat/client/default_tags.lua")
 
 include("easychat/client/font_extensions.lua")
 include("easychat/easychat.lua")
