cd78c41da293c200cd5070182122bbf0531d3026
Earu <earu.derg@gmail.com>
2019-12-26 17:47:48 +0100

    Dont forget to add expressions to client

--- lua/autorun/easychat_init.lua
+++ lua/autorun/easychat_init.lua
@@ -2,6 +2,7 @@ AddCSLuaFile("easychat/client/font_extensions.lua")
 AddCSLuaFile("easychat/easychat.lua")
 AddCSLuaFile("easychat/autoloader.lua")
 
+AddCSLuaFile("easychat/client/expressions.lua")
 AddCSLuaFile("easychat/client/chatbox_panel.lua")
 AddCSLuaFile("easychat/client/chat_tab.lua")
 AddCSLuaFile("easychat/client/settings_tab.lua")
