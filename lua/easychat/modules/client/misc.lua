01fd361ebce0571de2acc660276c98f8426f8b74
Earu <earu.derg@gmail.com>
2019-12-30 00:57:45 +0100

    Implement new chathud parts, fix scale part

--- lua/easychat/modules/client/misc.lua
+++ lua/easychat/modules/client/misc.lua
@@ -1,8 +1,8 @@
 local replaces = {
-		[".iunno"]     = [[¯\_(ツ)_/¯]],
-		[".lenny"]     = [[( ͡° ͜ʖ ͡° )]],
-		[".fliptable"] = [[(╯°□°）╯︵ ┻━┻]],
-		[".unflip "]   = [[┬─┬ ノ( ゜-゜ノ)]],
+	[".iunno"] = [[¯\_(ツ)_/¯]],
+	[".lenny"] = [[( ͡° ͜ʖ ͡° )]],
+	[".fliptable"] = [[(╯°□°）╯︵ ┻━┻]],
+	[".unflip "] = [[┬─┬ ノ( ゜-゜ノ)]]
 }
 
 local EC_EMOTES = CreateConVar("easychat_misc_emotes", "1", FCVAR_ARCHIVE, "Should we try to make sense of input when pressing tab")
@@ -11,52 +11,54 @@ local EC_REPLACE = CreateConVar("easychat_misc_replace", "1", FCVAR_ARCHIVE, "Al
 
 hook.Add("OnChatTab","EasyChatModuleMisc",function(text)
 	if EC_EMOTES:GetBool() then
-		local args = string.Explode(" ",text)
+		local args = string.Explode(" ", text)
 		if replaces[args[#args]] then
 			args[#args] = replaces[args[#args]]
 		end
-		return table.concat(args," ",1,#args)
+		return table.concat(args, " ", 1, #args)
 	end
 end)
 
 local stored = {}
-local store = function(ply,txt)
+local store = function(ply, txt)
 	if #stored >= 10 then
-		table.remove(stored,#stored)
+		table.remove(stored, #stored)
 	end
-	table.insert(stored,1,{
+	table.insert(stored, 1, {
 		Player = ply,
-		Content = txt,
+		Content = txt
 	})
 end
 
-hook.Add("OnPlayerChat","EasyChatModuleMisc",function(ply,txt)
-		if EC_GREENTEXT:GetBool() and string.match(txt,"^>") then
+hook.Add("OnPlayerChat", "EasyChatModuleMisc", function(ply, txt)
+	if EC_GREENTEXT:GetBool() and string.match(txt,"^>") then
 		chat.AddText(ply,Color(255,255,255),": ",Color(0,255,0),txt)
 		return true
 	end
 
-		if EC_REPLACE:GetBool() and string.match(txt,"^s%/.+%/.+") then
-		local args = string.Explode("/",txt)
+	if EC_REPLACE:GetBool() and string.match(txt, "^s%/.+%/.+") then
+		local args = string.Explode("/", txt)
 		local toreplace
-		if not args[2] or not args[3] then return end
-		for k,v in pairs(stored) do
-			if string.match(v.Content,string.PatternSafe(args[2])) and IsValid(v.Player) then
+		if not args[2] or not args[3] then
+			return
+		end
+		for k, v in pairs(stored) do
+			if string.match(v.Content, string.PatternSafe(args[2])) and IsValid(v.Player) then
 				toreplace = v
 				break
 			end
 		end
 		if toreplace then
-			local replaced = string.Replace(toreplace.Content,args[2],args[3])
+			local replaced = string.Replace(toreplace.Content, args[2], args[3])
 			if toreplace.Player ~= ply then
-				chat.AddText(ply,Color(255,255,255)," thinks ",toreplace.Player,Color(255,255,255)," meant: "..replaced)
+				chat.AddText(ply, Color(255, 255, 255), " thinks ", toreplace.Player, Color(255, 255, 255), " meant: " .. replaced)
 			else
-				chat.AddText(ply,Color(255,255,255)," meant: "..replaced)
+				chat.AddText(ply, Color(255, 255, 255), " meant: " .. replaced)
 			end
 			return true
 		end
 	else
-		store(ply,txt)
+		store(ply, txt)
 	end
 end)
 
