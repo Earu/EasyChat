4250f372403418436d16115a622068b6ec3d0b4a
Earu <earu.derg@gmail.com>
2020-01-09 15:16:37 +0100

    Less noise

--- lua/easychat/easychat.lua
+++ lua/easychat/easychat.lua
@@ -427,9 +427,9 @@ if CLIENT then
 			for i = 1, len, 3 do
 				counter =
 					math.fmod(counter * 8161, 4294967279) + -- 2^32 - 17: Prime!
-					(string.byte(text, i) * 16776193) +
-					((string.byte(text, i + 1) or (len - i + 256)) * 8372226) +
-					((string.byte(text, i + 2) or (len - i + 256)) * 3932164)
+					(text:byte(i) * 16776193) +
+					((text:byte(i + 1) or (len - i + 256)) * 8372226) +
+					((text:byte(i + 2) or (len - i + 256)) * 3932164)
 			end
 
 			return math.fmod(counter, 4294967291) -- 2^32 - 5: Prime (and different from the prime in the loop)
