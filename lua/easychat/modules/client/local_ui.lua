e402f531ea3a0bd224c7dc9e2d9f3c23530ea686
Earu <earu.derg@gmail.com>
2020-01-10 11:49:07 +0100

    cleanup

--- lua/easychat/modules/client/local_ui.lua
+++ lua/easychat/modules/client/local_ui.lua
@@ -4,38 +4,45 @@ panel.btnClose:Hide()
 panel.btnMaxim:Hide()
 panel.btnMinim:Hide()
 
-panel.Think = function(self)
+function panel:Think()
 	if not IsValid(EasyChat.GUI.ChatBox) then return end
-	local x,y,w,h = EasyChat.GUI.ChatBox:GetBounds()
-	self:SetPos(x + w,y)
+	local x, y, w, h = EasyChat.GUI.ChatBox:GetBounds()
+	self:SetPos(x + w, y)
 end
 
 panel:SetWide(150)
 panel.old_paint = panel.Paint
-panel.Paint = function(self,w,h)
+function panel:Paint(w, h)
 	if EasyChat.IsOpened() and EasyChat.Modes[EasyChat.Mode] and EasyChat.Modes[EasyChat.Mode].Name == "Local" then
 		if EasyChat.UseDermaSkin then
-			self.old_paint(self,w,h)
+			self:old_paint(w, h)
 		else
 			surface.SetDrawColor(EasyChat.OutlayColor)
-			surface.DrawRect(0,0,w,h)
+			surface.DrawRect(0, 0, w, h)
 			surface.SetDrawColor(EasyChat.OutlayOutlineColor)
-			surface.DrawOutlinedRect(0,0,w,h)
+			surface.DrawOutlinedRect(0, 0, w, h)
 		end
+
 		surface.SetFont("EasyChatFont")
-		surface.SetTextPos(15,5)
+		surface.SetTextPos(15, 5)
 		surface.SetTextColor(EasyChat.TextColor)
 		surface.DrawText("Message receivers")
-		local index = 1
-		for k,v in pairs(player.GetAll()) do
-			if v ~= LocalPlayer() and v:GetPos():Distance(LocalPlayer():GetPos()) <= GetConVar("easychat_local_msg_distance"):GetInt() then
-				surface.SetTextPos(15,25*index)
-				local col = team.GetColor(v:Team())
-				surface.SetTextColor(col.r,col.g,col.g,255)
-				local x,y = surface.GetTextSize(v:GetName())
-				self:SetTall((25 * index) + y + 10)
-				surface.DrawText(string.gsub(v:Nick(),"<.->",""))
-				index = index + 1
+
+		local i = 1
+		for _, ply in pairs(player.GetAll()) do
+			if ply ~= LocalPlayer()
+				and ply:GetPos():Distance(LocalPlayer():GetPos()) <= GetConVar("easychat_local_msg_distance"):GetInt()
+			then
+				local team_color = team.GetColor(ply:Team())
+
+				surface.SetTextPos(15, 25 * i)
+				surface.SetTextColor(team_color.r, team_color.g, team_color.g, 255)
+
+				local x, y = surface.GetTextSize(ply:GetName())
+				self:SetTall((25 * i) + y + 10)
+
+				surface.DrawText(string.gsub(ply:Nick(), "<.->", ""))
+				i = i + 1
 			end
 		end
 	end
--- lua/easychat/modules/darkrp.lua
+++ lua/easychat/modules/darkrp.lua
@@ -1,59 +1,60 @@
 if gmod.GetGamemode().Name ~= "DarkRP" then return end
 
 if CLIENT then
-	hook.Add("ECPostInitialize","EasyChatModuleDarkRP",function()
+	hook.Add("ECPostInitialize", "EasyChatModuleDarkRP", function()
 		local EC_ENABLE = GetConVar("easychat_enable")
 		local EC_TIMESTAMPS = GetConVar("easychat_timestamps")
 		local EC_TEAMS = GetConVar("easychat_teams")
 		local EC_TEAMS_COLOR = GetConVar("easychat_teams_colored")
 
-		GAMEMODE.OnPlayerChat = function(self,ply,msg,isteam,isdead,prefixtext,col1,col2) -- this is for the best
-			local tab = {}
+		-- this is for the best
+		function GAMEMODE:OnPlayerChat(ply, msg, is_team, is_dead, prefix, col1, col2)
+			local msg_components = {}
 
 			-- I don't trust this gamemode at all.
-			local col1 = col1 or Color(255,255,255)
-			local col2 = col2 or Color(255,255,255)
+			local col1 = col1 or Color(255, 255, 255)
+			local col2 = col2 or Color(255, 255, 255)
 
-			table.insert(tab,Color(255,255,255)) -- we don't want previous colors to be used again
+			table.insert(msg_components, Color(255, 255, 255)) -- we don't want previous colors to be used again
 
 			if EC_ENABLE:GetBool() then
 				if IsValid(ply) and EC_TEAMS:GetBool() then
 					if EC_TEAMS_COLOR:GetBool() then
 						local tcol = team.GetColor(ply:Team())
-						table.insert(tab,tcol)
+						table.insert(msg_components, tcol)
 					end
-					table.insert(tab,"["..team.GetName(ply:Team()).."] - ")
+					table.insert(msg_components, "[" .. team.GetName(ply:Team()) .. "] - ")
 				end
 			end
 
-			if isdead then
-				table.insert(tab,Color(240,80,80))
-				table.insert(tab,"*DEAD* " )
+			if is_dead then
+				table.insert(msg_components, Color(240, 80, 80))
+				table.insert(msg_components, "*DEAD* ")
 			end
 
-			if prefixtext then
+			if prefix then
 				if col1 == team.GetColor(ply:Team()) then -- Just prettier
-					col1 = Color(255,255,255)
+					col1 = Color(255, 255, 255)
 				end
 
-				table.insert(tab,col1)
+				table.insert(msg_components, col1)
 				-- Remove the nick appened, use our own system.
-				table.insert(tab,(string.gsub(prefixtext,string.PatternSafe(ply:Nick()),"")))
+				table.insert(msg_components, (prefix:gsub(ply:Nick():PatternSafe(), "")))
 			end
 
-			if IsValid(ply)  then
-				table.insert(tab,ply)
+			if IsValid(ply) then
+				table.insert(msg_components, ply)
 			else
-				table.insert(tab,Color(110,247,177))
-				table.insert(tab,"???") -- console or weird stuff
+				table.insert(msg_components, Color(110, 247, 177))
+				table.insert(msg_components, "???") -- console or weird stuff
 			end
 
-			table.insert(tab,Color(255,255,255))
-			table.insert(tab,": ")
-			table.insert(tab,col2)
-			table.insert(tab,msg)
+			table.insert(msg_components, Color(255, 255, 255))
+			table.insert(msg_components, ": ")
+			table.insert(msg_components, col2)
+			table.insert(msg_components, msg)
 
-			chat.AddText(unpack(tab))
+			chat.AddText(unpack(msg_components))
 
 			return true
 		end
