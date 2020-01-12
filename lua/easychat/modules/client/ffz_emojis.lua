3789e9deadfb36a2227faf31f076481d0d4c4e38
Earu <earu.derg@gmail.com>
2020-01-13 00:54:47 +0100

    Add new twitch emote providers, courtesy of @cynosphere

---
+++ lua/easychat/modules/client/ffz_emojis.lua
@@ -0,0 +1,120 @@
+local function material_data(mat)
+	return Material("../data/" .. mat)
+end
+
+local UNCACHED = false
+local PROCESSING = true
+
+local cache = {}
+
+local FOLDER = "ffz"
+file.CreateDir(FOLDER, "DATA")
+
+local LOOKUP_TABLE_URL = "https://api.frankerfacez.com/v1/set/global"
+local lookup = {}
+http.Fetch(LOOKUP_TABLE_URL, function(body)
+	local tbl = util.JSONToTable(body)
+	for _, set in pairs(tbl.sets) do
+		for _, v in pairs(set.emoticons) do
+			local name = v.name
+			lookup[name] = v.urls[4] and v.urls[4] or (v.urls[2] and v.urls[2] or v.urls[1])
+			cache[name] = UNCACHED
+		end
+	end
+end, function(err)
+	Msg("[Emoticons] ")
+	print("Could not get the lookup table for FFZ")
+end)
+
+local function get_ffz_url(name)
+	return "https:" .. lookup[name]
+end
+
+local function get_ffz(name)
+	if not lookup[name] then
+		return false
+	end
+
+	local c = cache[name]
+	if c then
+		if c == true then
+			return
+		end
+		return c
+	else
+		if c == nil then
+			return false
+		end
+	end
+
+	-- Otherwise download dat shit
+	cache[name] = PROCESSING
+
+	local path = FOLDER .. "/" .. name .. ".png"
+
+	local exists = file.Exists(path, "DATA")
+	if exists then
+		local mat = material_data(path)
+
+		if not mat or mat:IsError() then
+			Msg("[Emoticons] ")
+			print("Material found, but is error: ", name, "redownloading")
+		else
+			c = mat
+			cache[name] = c
+			return c
+		end
+	end
+
+	local url = get_ffz_url(name)
+
+	local function fail(err, isvariant)
+		Msg("[Emoticons] ")
+		print("Http fetch failed for", url, ": " .. tostring(err))
+
+		-- bad hack
+		if not isvariant then
+			Msg("[Emoticons] ")
+			print("Retrying without variant selector just in case...")
+			http.Fetch(url:Replace("-fe0f.png",".png"), function(data, len, hdr, code)
+				if code ~= 200 or len <= 222 then
+					return fail(code)
+				end
+
+				file.Write(path, data)
+
+				local mat = material_data(path)
+
+				if not mat or mat:IsError() then
+					Msg("[Emoticons] ")
+					print("Downloaded material, but is error: ", name)
+					return
+				end
+
+				cache[name] = mat
+			end, function(e) fail(e, true) end)
+		end
+	end
+
+	http.Fetch(url, function(data, len, hdr, code)
+		if code ~= 200 or len <= 222 then
+			return fail(code)
+		end
+
+		file.Write(path, data)
+
+		local mat = material_data(path)
+
+		if not mat or mat:IsError() then
+			Msg("[Emoticons] ")
+			print("Downloaded material, but is error: ", name)
+			return
+		end
+
+		cache[name] = mat
+	end, fail)
+end
+
+EasyChat.ChatHUD:RegisterEmoteProvider("ffz", get_ffz, 1)
+
+return "FrankerFaceZ Global Emotes"
