5a4553d955535414050135898f7babce6ed44fdc
Earu <earu.derg@gmail.com>
2020-02-04 13:32:05 +0100

    Sort files a bit

--- lua/easychat/modules/client/steam_emojis.lua
+++ lua/easychat/modules/client/steam_emojis.lua
@@ -14,28 +14,6 @@ local function count(a, n)
 	return x
 end
 
-local BASE64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-local function base64_decode(data)
-	data = string.gsub(data, "[^" .. BASE64 .. "=]", "")
-	return (data:gsub(".", function(x)
-		if (x == "=") then return "" end
-		local r, f = "", (BASE64:find(x) - 1)
-		for i = 6, 1, -1 do
-			r = r .. (f % 2^i - f % 2^(i-1) > 0 and "1" or "0")
-		end
-
-		return r
-	end):gsub("%d%d%d?%d?%d?%d?%d?%d?", function(x)
-		if (#x ~= 8) then return "" end
-		local c = 0
-		for i=1, 8 do
-			c = c + (x:sub(i, i) == "1" and 2^(8 - i) or 0)
-		end
-
-		return string.char(c)
-	end))
-end
-
 local FOLDER = "easychat/emojis/steam"
 file.CreateDir(FOLDER, "DATA")
 
@@ -146,7 +124,7 @@ local function get_steam_emote(name)
 			return fail("sub")
 		end
 
-		data = base64_decode(data)
+		data = EasyChat.DecodeBase64(data)
 		if not data or data == "" then
 			return fail("Base64Decode")
 		end
