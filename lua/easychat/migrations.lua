e20495981ea2a37701c06207d3b5437ab607f89b
Earu <earu.derg@gmail.com>
2020-03-07 16:38:09 +0100

    Upgrade migrations to follow timestamp_name format and sort them

--- lua/easychat/migrations.lua
+++ lua/easychat/migrations.lua
@@ -1,14 +1,39 @@
 local MIGRATIONS_DIRECTORY = "easychat/migrations"
 
-if SERVER then
-	for _, f in pairs((file.Find(MIGRATIONS_DIRECTORY .. "/*.lua", "LUA"))) do
-		local file_path = ("%s/%s"):format(MIGRATIONS_DIRECTORY, f)
-		AddCSLuaFile(file_path)
+local files = (file.Find(MIGRATIONS_DIRECTORY .. "/*.lua", "LUA"))
+local migrations = {}
+for _, f in pairs(files) do
+	local file_path = ("%s/%s"):format(MIGRATIONS_DIRECTORY, f)
+	local timestamp = tonumber(f:match("^(%d+)_"))
+	if timestamp then
+		migrations[timestamp] = file_path
+	else
+		local err = ("[ECMigrations] > migration \'%s\' did not have a timestamp and was discarded"):format(f)
+		ErrorNoHalt(err)
 	end
 end
 
--- for these to be ran in the correct order you must always specify a number in front of the file name
-for _, f in pairs((file.Find(MIGRATIONS_DIRECTORY .. "/*.lua", "LUA"))) do
-	local file_path = ("%s/%s"):format(MIGRATIONS_DIRECTORY, f)
-	include(file_path)
+table.sort(migrations)
+
+if SERVER then
+	for migration_timestamp, file_path in pairs(migrations) do
+		AddCSLuaFile(file_path)
+		local cookie_name = ("EC_MIGRATION_%d"):format(migration_timestamp)
+		if not cookie.GetNumber(cookie_name) then
+			print(("[ECMigrations - SV] > Running %s"):format(file_path))
+			include(file_path)
+			cookie.Set(cookie_name, "1")
+		end
+	end
+end
+
+if CLIENT then
+	for migration_timestamp, file_path in pairs(migrations) do
+		local cookie_name = ("EC_MIGRATION_%d"):format(migration_timestamp)
+		if not cookie.GetNumber(cookie_name) then
+			print(("[ECMigrations - CL] > Running %s"):format(file_path))
+			include(file_path)
+			cookie.Set(cookie_name, "1")
+		end
+	end
 end
