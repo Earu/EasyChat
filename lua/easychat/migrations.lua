2daa78738c2696a4abc6c83d9c646623015bd42b
Earu <earu.derg@gmail.com>
2020-03-07 16:52:24 +0100

    Actually sort migrations properly

--- lua/easychat/migrations.lua
+++ lua/easychat/migrations.lua
@@ -6,19 +6,21 @@ for _, f in pairs(files) do
 	local file_path = ("%s/%s"):format(MIGRATIONS_DIRECTORY, f)
 	local timestamp = tonumber(f:match("^(%d+)_"))
 	if timestamp then
-		migrations[timestamp] = file_path
+		table.insert(migrations, { timestamp, file_path })
 	else
 		local err = ("[ECMigrations] > migration \'%s\' did not have a timestamp and was discarded"):format(f)
 		ErrorNoHalt(err)
 	end
 end
 
-table.sort(migrations)
+-- sort by timestamp
+table.sort(migrations, function(a, b) a[1] > b[1] end)
 
 if SERVER then
-	for migration_timestamp, file_path in pairs(migrations) do
+	for _, migration in ipairs(migrations) do
+		local timestamp, file_path = migration[1], migration[2]
 		AddCSLuaFile(file_path)
-		local cookie_name = ("EC_MIGRATION_%d"):format(migration_timestamp)
+		local cookie_name = ("EC_MIGRATION_%d"):format(timestamp)
 		if not cookie.GetNumber(cookie_name) then
 			print(("[ECMigrations - SV] > Running %s"):format(file_path))
 			include(file_path)
@@ -28,8 +30,9 @@ if SERVER then
 end
 
 if CLIENT then
-	for migration_timestamp, file_path in pairs(migrations) do
-		local cookie_name = ("EC_MIGRATION_%d"):format(migration_timestamp)
+	for _, migration in ipairs(migrations) do
+		local timestamp, file_path = migration[1], migration[2]
+		local cookie_name = ("EC_MIGRATION_%d"):format(timestamp)
 		if not cookie.GetNumber(cookie_name) then
 			print(("[ECMigrations - CL] > Running %s"):format(file_path))
 			include(file_path)
