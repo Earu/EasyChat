07e4dc144504bb192f9d6007f7e1e0dd8276d236
Earu <earu.derg@gmail.com>
2020-04-10 19:55:54 +0200

    Less verbose initialization logs

--- lua/easychat/modules/server/murder.lua
+++ lua/easychat/modules/server/murder.lua
@@ -1,7 +1,9 @@
-if gmod.GetGamemode().Name ~= "Murder" then return end
+if gmod.GetGamemode().Name ~= "Murder" then return "Murder Compat" end
 
 hook.Add("PlayerSay","EasyChatMurderWorkaround",function(ply, msg)
 	if ply:Team() ~= 2 or not ply:Alive() or GAMEMODE:GetRound() == 0 then
 		return msg
 	end
 end)
+
+return "Murder Compat"
