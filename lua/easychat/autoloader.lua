8c2accf51dcad791295deac62caa5e3abc632244
Earu <earu.derg@gmail.com>
2020-04-10 19:55:54 +0200

    Less verbose initialization logs

--- lua/easychat/autoloader.lua
+++ lua/easychat/autoloader.lua
@@ -5,45 +5,36 @@ local modules = {}
 local color_good = Color(0, 160, 220)
 local color_bad = Color(255, 127, 127)
 local color_default = Color(244, 167, 66)
+local module_loaded_count, module_failed_count = 0, 0
 
-local function is_string_return(arg)
-	return type(arg) == "string" and arg or nil
-end
-
-local function prettify_name(str)
-	return str:gsub(".lua", ""):SetChar(1, str[1]:upper())
-end
-
-local function add_module(name, file_name, callback, where)
-	local pretty_file_name = prettify_name(file_name)
-	local name = is_string_return(name) or pretty_file_name
+local function add_module(name, file_name, callback)
+	local name = isstring(name) and name or file_name
 	table.insert(modules, {
 		Name = name,
-		File = pretty_file_name,
+		File = file_name,
 		Callback = callback
 	})
 
-	local to_print = is_string_return(name) or pretty_file_name
-	MsgC(color_default, "[ECModule - " .. where .. "] ⮞ ", color_good, to_print .. " loaded\n")
+	module_loaded_count = module_loaded_count + 1
 end
 
 local function module_error(file_name, err, where)
-	MsgC(color_default, "[ECModule - " .. where .. "] ⮞ ", color_bad, "Couldn't load " .. file_name .. "\n " .. err .. "\n")
+	MsgC(color_default, "[EasyChat | " .. where .. "] ⮞ ", color_bad, "Couldn't load " .. file_name .. "\n")
+	ErrorNoHalt(err)
+
+	module_failed_count = module_failed_count + 1
 end
 
 local function load_modules(path)
 	local path = path or EC_MODULE_PATH
-	local log_separator = "-----------------------------------------------\n"
 
-	MsgC(color_default, "- EasyChat -\n")
-	MsgC(color_default, log_separator)
-
+	local start_time = SysTime()
 	for _, file_name in pairs((file.Find(path .. "*.lua", "LUA"))) do
 		AddCSLuaFile(path .. file_name)
 		local module = CompileFile(path .. file_name)
 		local succ, err = pcall(module)
 		if succ then
-			add_module(err, file_name, module, "SH")
+			add_module(err, file_name, module)
 		else
 			module_error(file_name, err, "SH")
 		end
@@ -54,7 +45,7 @@ local function load_modules(path)
 			local module = CompileFile(path .. "server/" .. file_name)
 			local succ, err = pcall(module)
 			if succ then
-				add_module(err, file_name, module, "SV")
+				add_module(err, file_name, module)
 			else
 				module_error(file_name, err, "SV")
 			end
@@ -70,17 +61,19 @@ local function load_modules(path)
 			local module = CompileFile(path .. "client/" .. file_name)
 			local succ, err = pcall(module)
 			if succ then
-				add_module(err, file_name, module, "CL")
+				add_module(err, file_name, module)
 			else
 				module_error(file_name, err, "CL")
 			end
 		end
 	end
 
-	MsgC(color_default, log_separator)
+	MsgC(color_default, "[EasyChat] ⮞ ", color_good, ("Loaded %d modules successfully, %d modules failed to load.\n"):format(module_loaded_count, module_failed_count))
+	MsgC(color_default, "[EasyChat] ⮞ ", color_good, ("Initialized in %fs\n"):format(SysTime() - start_time))
+	module_loaded_count, module_failed_count = 0, 0
 end
 
-local get_modules = function()
+local function get_modules()
 	return modules
 end
 
