b46f4bd2c0028a891365f5246332748b23d8ee27
Earu <earu.derg@gmail.com>
2020-04-17 13:09:56 +0200

    Make text more readable in the chat gui

--- lua/easychat/client/vgui/richtextx.lua
+++ lua/easychat/client/vgui/richtextx.lua
@@ -12,6 +12,10 @@ function PANEL:Init()
 				html, body, span {
 					padding: 0;
 					margin: 0;
+					text-shadow: -1px 1px 4px #000,
+						1px 1px 4px #000,
+				   		1px -1px 4px #000;
+				  		-1px -1px 4px #000;
 				}
 
 				::selection {
