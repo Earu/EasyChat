0109802590a0dc7d585cffb2d8bb7fdc6528477f
Earu <earu.derg@gmail.com>
2020-05-07 01:51:09 +0200

    Fix a typo

--- lua/easychat/modules/client/mentions.lua
+++ lua/easychat/modules/client/mentions.lua
@@ -160,7 +160,8 @@ hook.Add("OnPlayerChat", "EasyChatModuleMention", function(ply, msg, is_team, is
 
 	msg = msg:lower()
 	local undec_nick = undecorate_nick(lp:Nick()):PatternSafe()
-	if not msg:match("^[%!%.%/]") and #msg:match(undec_nick) > 1 then
+	local match = msg:match(undec_nick)
+	if not msg:match("^[%!%.%/]") and #match > 1 then
 		if EC_MENTION_FLASH:GetBool() then
 			system.FlashWindow()
 		end
