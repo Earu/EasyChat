7b23cb7aae4c51e0629af9d7d6941dd0743a46aa
Earu <earu.derg@gmail.com>
2020-05-11 12:38:27 +0200

    Prevent weird language inputs in translation settings

--- lua/easychat/client/translator.lua
+++ lua/easychat/client/translator.lua
@@ -5,6 +5,26 @@ local translator = {
 	CurrentID = 1
 }
 
+local language_lookup = {
+	["Automatic"] = "auto",
+
+	["Afrikaans"] = "af", ["Irish"] = "ga", ["Albanian"] = "sq", ["Italian"] = "it", ["Arabic"] = "ar", ["Japanese"] = "ja",
+	["Azerbaijani"] = "az", ["Kannada"] = "kn", ["Basque"] = "eu", ["Korean"] = "ko", ["Bengali"] = "bn", ["Latin"] = "la",
+	["Belarusian"] = "be", ["Latvian"] = "lv", ["Bulgarian"] =	"bg", ["Lithuanian"] = "lt", ["Catalan"] = "ca",
+	["Macedonian"] = "mk", ["Chinese Simplified"] = "zh-CN", ["Malay"] =	"ms", ["Chinese Traditional"] = "zh-TW", ["Maltese"] = "mt",
+	["Croatian"] = "hr", ["Norwegian"] = "no", ["Czech"] = "cs", ["Persian"] = "fa", ["Danish"] = "da", ["Polish"] = "pl", ["Dutch"] = "nl",
+	["Portuguese"] = "pt", ["English"] = "en", ["Romanian"] = "ro", ["Esperanto"] =	"eo", ["Russian"] = "ru", ["Estonian"] = "et", ["Serbian"] = "sr",
+	["Filipino"] = "tl", ["Slovak"] = "sk", ["Finnish"] = "fi", ["Slovenian"] =	"sl", ["French"] = "fr", ["Spanish"] = "es", ["Galician"] = "gl",
+	["Swahili"] = "sw", ["Georgian"] = "ka", ["Swedish"] = "sv", ["German"] = "de", ["Tamil"] =	"ta", ["Greek"] = "el", ["Telugu"] = "te",
+	["Gujarati"] = "gu", ["Thai"] = "th", ["Haitian Creole"] = "ht", ["Turkish"] = "tr", ["Hebrew"] = "iw", ["Ukrainian"] =	"uk", ["Hindi"] = "hi",
+	["Urdu"] = "ur", ["Hungarian"] = "hu", ["Vietnamese"] = "vi", ["Icelandic"] = "is", ["Welsh"] = "cy", ["Indonesian"] = "id", ["Yiddish"] = "yi",
+}
+
+local valid_languages = {}
+for _, country_code in pairs(language_lookup) do
+	valid_languages[country_code] = true
+end
+
 local red_col = Color(255, 0, 0)
 local function create_translation_panel(self)
 	local tr_panel = vgui.Create("DHTML")
@@ -83,6 +103,11 @@ function translator:Destroy()
 end
 
 function translator:Translate(text, source_lang, target_lang, on_finish)
+	if not valid_languages[source_lang] or not valid_languages[target_lang] then
+		on_finish(false)
+		return
+	end
+
 	if cached_translations[text] and cached_translations[text][target_lang] then
 		on_finish(true, text, cached_translations[text][target_lang])
 		return
