370823d60ad182b179a8c97aceca29ca98757272
Earu <earu.derg@gmail.com>
2020-09-04 15:43:43 +0200

    Add the last time a player was seen to the join notifications

--- lua/easychat/modules/join_leave.lua
+++ lua/easychat/modules/join_leave.lua
@@ -19,7 +19,22 @@ if SERVER then
 		net.WriteString(ply:Nick())
 		net.WriteInt(ply:Team(), 32)
 		net.WriteString(ply:SteamID())
+
+		local last_seen = ply:GetPData("ECLastSeen", -1)
+		local cur_time = os.time()
+		if last_seen ~= -1 then
+			local last_seen_time = tonumber(last_seen) or 0
+			local time_diff = last_seen_time ~= 0 and (cur_time - last_seen_time) or 0
+
+			net.WriteInt(time_diff, 32)
+			net.WriteInt(cur_time, 32)
+			net.WriteInt(last_seen_time, 32)
+		else
+			net.WriteInt(-1, 32)
+		end
+
 		net.Broadcast()
+		ply:SetPData("ECLastSeen", cur_time)
 	end)
 
 	hook.Add("player_disconnect", TAG, function(data)
@@ -46,8 +61,11 @@ end
 if CLIENT then
 	local green_color = Color(100, 230, 100)
 	local red_color = Color(230, 100, 100)
+	local cyan_color = Color(0, 255, 255)
+	local teal_color = Color(0, 255, 150)
 	local gray_color = Color(200, 200, 200)
 	local white_color = Color(255, 255, 255)
+	local black_color = Color(0, 0, 0)
 
 	hook.Add("ChatText", TAG, function(_, _, _, mode)
 		if not EC_JOIN_LEAVE:GetBool() then return end
@@ -114,15 +132,44 @@ if CLIENT then
 		end
 
 		local network_id = net.ReadString()
+		local last_seen_diff = net.ReadInt(32)
+
+		local last_seen_time, cur_seen_time
+		local seen_date, formatted_diff
+		if last_seen_diff ~= -1 then
+			last_seen_time = net.ReadInt(32)
+			cur_seen_time = net.ReadInt(32)
+
+			if os.date("%D", last_seen_time) == os.date("%D", cur_seen_time) then
+				seen_date = "today"
+			elseif os.date("%D", last_seen_time) == os.date("%D", cur_seen_time - 86400) then
+				seen_date = "yesterday"
+			else
+				seen_date = os.date("%D", last_seen_time)
+			end
+
+			formatted_diff = (" (%s ago)"):format(string.NiceTime(last_seen_diff))
+		end
+
 		local ply_col = team.GetColor(team_id)
 		if EC_PLAYER_PASTEL:GetBool() then
 			ply_col = EasyChat.PastelizeNick(name)
 		end
 
+		local formatted_id = (" (%s) "):format(network_id)
 		if is_join then
-			chat.AddText(green_color, " ● ", ply_col, name, gray_color, " (" .. network_id .. ") ", white_color, "has ", green_color, "spawned")
+			chat.AddText(green_color, " ● ", ply_col, name, gray_color, formatted_id, white_color, "has ", green_color, "spawned")
+			if last_seen_diff == -1 then
+				chat.AddText(black_color, " ▸ ", white_color, "Joined for the ", cyan_color, "first time", white_color, "!")
+			else
+				chat.AddText(black_color, " ▸ ", white_color, "Last seen ", cyan_color, seen_date, white_color, " at ", teal_color, os.date("%H:%M", last_seen_time), gray_color, formatted_diff)
+			end
 		else
-			chat.AddText(red_color, " ● ", ply_col, name, gray_color, " (" .. network_id .. ") ", white_color, "has ", red_color, "left", white_color, " the server", red_color, " (" .. reason .. ")")
+			if reason == "Gave up connecting" then
+				chat.AddText(red_color, " ● ", ply_col, name, gray_color, formatted_id, red_color, "gave up", white_color, " connecting")
+			else
+				chat.AddText(red_color, " ● ", ply_col, name, gray_color, formatted_id, white_color, "has ", red_color, "left", white_color, " the server", red_color, " (" .. reason .. ")")
+			end
 		end
 	end)
 
