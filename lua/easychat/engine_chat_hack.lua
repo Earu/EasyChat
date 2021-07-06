c42c315b719d5c91edae4e83a3aa403be59bb30c
Earu <earu.derg@gmail.com>
2021-07-06 09:48:27 +0200

    okay this will work

--- lua/easychat/engine_chat_hack.lua
+++ lua/easychat/engine_chat_hack.lua
@@ -16,6 +16,15 @@ if SERVER then
 		hook.NativeAdd(event_name, identifier, callback)
 	end
 
+	hook.NativeCall = hook.NativeCall or hook.Call
+	function hook.Call(event_name, ...)
+		if event_name == "PlayerSay" then
+			event_name = "ECPlayerSay"
+		end
+
+		hook.NativeCall(event_name, ...)
+	end
+
 	-- handle messages that are run by the engine (usually the say or sayteam commands)
 	hook.NativeAdd("PlayerSay", TAG, function(ply, msg, is_team, is_local)
 		if not IsValid(ply) then return end -- for console just let source handle it I guess
