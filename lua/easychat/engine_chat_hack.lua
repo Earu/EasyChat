1d57592e138c7c8e69fb9f60c90471006ac76894
Earu <earu.derg@gmail.com>
2021-07-25 19:33:39 +0200

    cleanup

--- lua/easychat/engine_chat_hack.lua
+++ lua/easychat/engine_chat_hack.lua
@@ -11,7 +11,7 @@ if SERVER then
 	end
 
 	if not has_slog and has_sourcenet then
-		has_sourcenet = pcall(require, "sourcenet") 
+		has_sourcenet = pcall(require, "sourcenet")
 		if has_sourcenet then
 			include("sourcenet/incoming.lua")
 
@@ -44,7 +44,7 @@ if SERVER then
 	end
 
 	if not has_slog and not has_sourcenet then
-		EasyChat.Print("Could not find a proper installation of slog or sourcenet,\n" 
+		EasyChat.Print("Could not find a proper installation of slog or sourcenet, "
 			.. "chat console commands will use the engine networking as a result.\n"
 			.. "If you care about console commands please install either of these two:\n"
 			.. "- https://github.com/danielga/gm_sourcenet\n- https://github.com/Heyter/gbins/tree/master/slog/src")
