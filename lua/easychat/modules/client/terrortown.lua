f3878d0e259e7637146b75e08b694904d7442904
Python1320 <python1320+gh@gmail.com>
2023-10-05 12:23:43 +0300

    Use engine.ActiveGamemode() instead of Name (#114)

--- lua/easychat/modules/client/terrortown.lua
+++ lua/easychat/modules/client/terrortown.lua
@@ -1,7 +1,7 @@
-if gmod.GetGamemode().Name ~= "terrortown" then return "Terrortown Compat" end
-
-hook.Add("ECVoiceHUD", "EasyChatModuleTerrortown", function()
-	return false -- DISABLED: TTT Voice HUD affects traitor voice mode
-end)
+if engine.ActiveGamemode() == "terrortown" then
+	hook.Add("ECVoiceHUD", "EasyChatModuleTerrortown", function()
+		return false -- DISABLED: TTT Voice HUD affects traitor voice mode
+	end)
+end
 
 return "Terrortown Compat"
